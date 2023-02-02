f26ec81d190b06b17d3c4d0fdf11dc1822b25b49
baranselgok <59847659+baranselgok@users.noreply.github.com>
2023-02-02 15:18:14 +0300

    Create Burc.java

---
+++ Burc.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Burc {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int month, day;
+        System.out.print("Month of birth: ");
+        month = inp.nextInt();
+
+        System.out.print("day of birth: ");
+        day = inp.nextInt();
+
+        if (month == 1) {
+            if (day < 22) System.out.println("Your horoscope is:" + "capricorn");
+            else System.out.println("Your horoscope is:" + "aquarius");
+        }
+        if (month == 2) {
+            if (day < 20) System.out.println("Your horoscope is:" + "aquarius");
+            else System.out.println("Your horoscope is:" + "pisces");
+        }
+        if (month == 3) {
+            if (day < 21) System.out.println("Your horoscope is:" + "pisces");
+            else System.out.println("Your horoscope is:" + "aries");
+        }
+        if (month == 4) {
+            if (day < 21) System.out.println("Your horoscope is:" + "aries");
+            else System.out.println("Your horoscope is:" + "taurus");
+        }
+        if (month == 5) {
+            if (day < 22) System.out.println("Your horoscope is:" + "taurus");
+            else System.out.println("Your horoscope is:" + "gemini");
+        }
+        if (month == 6) {
+            if (day < 23) System.out.println("Your horoscope is:" + "gemini");
+            else System.out.println("Your horoscope is:" + "cancer");
+        }
+        if (month == 7) {
+            if (day < 23) System.out.println("Your horoscope is:" + "cancer");
+            else System.out.println("Your horoscope is:" + "leo");
+        }
+        if (month == 8) {
+            if (day < 24) System.out.println("Your horoscope is:" + "leo");
+            else System.out.println("Your horoscope is:" + "virgo");
+        }
+        if (month == 9) {
+            if (day < 24) System.out.println("Your horoscope is:" + "virgo");
+            else System.out.println("Your horoscope is:" + "scales");
+        }
+        if (month == 10) {
+            if (day < 24) System.out.println("Your horoscope is:" + "scales");
+            else System.out.println("Your horoscope is:" + "scorpio");
+        }
+        if (month == 11) {
+            if (day < 24) System.out.println("Your horoscope is:" + "scorpio");
+            else System.out.println("Your horoscope is:" + "sagittarius");
+        }
+        if (month == 12) {
+            if (day < 23) System.out.println("Your horoscope is:" + "sagittarius");
+            else System.out.println("Your horoscope is:" + "capricorn");
+        }
+        else System.out.print("Wrong number of month.");
+    }
+}
