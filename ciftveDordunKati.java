614523107a527830d587dcb5d30cbcdbb84dae35
baranselgok <59847659+baranselgok@users.noreply.github.com>
2023-02-03 17:12:34 +0300

    Create ciftveDordunKati.java

---
+++ ciftveDordunKati.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ciftveDordunKati {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int i, toplam = 0;
+        do {
+            System.out.print("Sayi giriniz: ");
+            i = inp.nextInt();
+
+            if (i%4 == 0){
+                toplam += i;
+            }
+        }
+        while (i%2 == 0);
+        System.out.print("Toplam = " + toplam);
+    }
+}
