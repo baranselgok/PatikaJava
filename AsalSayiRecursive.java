75e5624c28e628a9caa90fdad2b2075138092470
baranselgok <59847659+baranselgok@users.noreply.github.com>
2023-02-05 18:42:57 +0300

    Create AsalSayiRecursive.java

---
+++ AsalSayiRecursive.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AsalSayiRecursive {
+
+    public static boolean asalsayi(int n, int i) {
+        if (n <= 1) {
+            return false;
+        }
+        if (i == 1) {
+            return true;
+        }
+        if (n % i == 0) {
+            return false;
+        }
+        return asalsayi(n, i - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayi giriniz:");
+        int n = scan.nextInt();
+        int i = n / 2;
+        if (asalsayi(n, i)) {
+            System.out.println(n + " asal sayıdır .");
+        } else {
+            System.out.println(n + " asal değildir.");
+        }
+    }
+
+}
